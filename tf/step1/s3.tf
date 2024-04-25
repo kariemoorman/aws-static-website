d22050dc6cbb6706f82d9a96f8fbace46fd236f8
karie <25291562+kariemoorman@users.noreply.github.com>
2024-04-24 22:12:37 -0700

    Update s3.tf

--- tf/step1/s3.tf
+++ tf/step1/s3.tf
@@ -12,10 +12,10 @@ resource "aws_s3_bucket" "week1-bucket" {
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.week1-bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # Add S3 Bucket Versioning
